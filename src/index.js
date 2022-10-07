43fb43fb83bb5ec9393306a5dca08bd9f1319486
github-username <mail@gmail.com>
2022-10-07 21:41:38 +0200

    refactor: change contact desktop image path

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+import "./scss/main.scss";
+
 import { validate } from "./js/formValidation.js";
 import { changeScrollPosition } from "./js/scroll.js";
+import { addClickHandler } from "./js/tags.js";
---
+++ src/js/tags.js
@@ -0,0 +1,49 @@
+export { addClickHandler };
+
+const addClickHandler = () => {
+  document.querySelector(".projects__tags").addEventListener("click", (e) => {
+    if (e.target.classList.contains("tag")) {
+      let clickedTag = e.target;
+      removeSelectedTag();
+      selectClikedTag(clickedTag);
+      if (clickedTag.textContent === "All") {
+        showAllProjects();
+      } else {
+        filterProjectsBySelectedTag(clickedTag.innerText);
+      }
+    }
+  });
+};
+
+const removeSelectedTag = () => {
+  let tags = document.querySelectorAll(".projects__tags .tag");
+  tags.forEach((tag) => {
+    tag.classList.remove("tag--active");
+    tag.classList.add("tag--bordered");
+  });
+};
+const selectClikedTag = (clickedTag) => {
+  clickedTag.classList.remove("tag--bordered");
+  clickedTag.classList.add("tag--active");
+};
+
+const showAllProjects = () => {
+  let projects = document.querySelectorAll(".projects-content .project");
+  projects.forEach((project) => {
+    project.classList.remove("project__hidden");
+  });
+};
+
+const filterProjectsBySelectedTag = (selectedTag) => {
+  let projects = document.querySelectorAll(".projects-content .project");
+  projects.forEach((project) => {
+    project.classList.add("project__hidden");
+    project.querySelectorAll(".tag").forEach((tag) => {
+      if (tag.textContent === selectedTag) {
+        project.classList.remove("project__hidden");
+      } else console.log("Something Doesn't work");
+    });
+  });
+};
+
+addClickHandler();
